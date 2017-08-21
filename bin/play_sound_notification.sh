4d14e387ceba5d4d8af15e97138b62a1171a98a7
Ezequiel Barrios <ezequiel@barrios.com.ar>
2017-08-21 05:45:58 -0300

    blurlock

--- bin/play_sound_notification.sh
+++ bin/play_sound_notification.sh
@@ -1,3 +1,10 @@
 #!/bin/sh -e
 # Requires mplayer
+if [ -f /usr/bin/mplayer ]; then
 /usr/bin/mplayer --quiet ~/share/sounds/game_alert.mp3 > /dev/null
+fi
+
+# Requires mpg123
+if [ -f /usr/bin/mpg123 ]; then
+/usr/bin/mpg123 -q ~/share/sounds/game_alert.mp3 > /dev/null
+fi
