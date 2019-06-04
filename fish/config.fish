d0d8619b4680122051370eddfb998fba513de09b
Ezequiel Barrios <ezequiel@barrios.com.ar>
2019-06-04 14:43:17 +0900

    fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,10 @@
-alias cat="bat"
-alias catplain="bat --style=plain"
-alias cat2="/usr/bin/cat"
-set -x BAT_THEME "Monokai Extended Light"
+# Environment
+set -x BAT_THEME "Monokai Extended Bright"
 set -x DOTFILES ~/repositorios/dotfiles
+
+# Alias / Abbr
+#alias cat="bat"
+abbr -a catplain bat --style=plain
+#alias cat2="/usr/bin/cat"
+abbr -a pacmans sudo powerpill
+abbr -a pacman sudo pacman
